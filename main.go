50930d7ec5e32410a19a882cbf18ac1d1de41e63
stu-smith-01 <stuart.sds.smith@gmail.com>
2026-02-26 17:24:18 +0000

    add Client and Broker structs add http routing for home and ws routes add run method for the broker

--- main.go
+++ main.go
@@ -1,3 +1,67 @@
 package main
 
-func main() {}
+import "net/http"
+
+type Client struct {
+	broker *Broker
+	send   chan []byte
+}
+type Broker struct {
+	// Inbound messages from clients.
+	broadcast chan []byte
+
+	// Registered clients.
+	clients map[*Client]bool
+
+	// Register clients.
+	register chan *Client
+}
+
+func NewClient() *Client {
+	return &Client{}
+}
+
+func NewBroker() *Broker {
+	return &Broker{
+		broadcast: make(chan []byte),
+		clients:   make(map[*Client]bool),
+		register:  make(chan *Client),
+	}
+}
+
+func (b *Broker) run() {
+
+	for {
+		select {
+		case client := <-b.register:
+			b.clients[client] = true
+		case message := <-b.broadcast:
+			for client := range b.clients {
+				select {
+				case client.send <- message:
+				default:
+					close(client.send)
+					delete(b.clients, client)
+				}
+
+			}
+		}
+	}
+
+}
+
+func main() {
+	broker := NewBroker()
+	go broker.run()
+
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(broker, w, r)
+	})
+	http.ListenAndServe("1111", nil)
+}
+
+func serveWs(broker *Broker, w http.ResponseWriter, r *http.Request) {}
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "home.html")
+}
